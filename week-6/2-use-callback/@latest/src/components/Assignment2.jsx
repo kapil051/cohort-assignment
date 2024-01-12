08aa8c892f1f778cbd3d71518ad6212800485120
kapil kant <kapilkant471@gmail.com>
2024-01-12 10:18:24 +0530

    usecallBack assignment2 completed

--- week-6/2-use-callback/@latest/src/components/Assignment2.jsx
+++ week-6/2-use-callback/@latest/src/components/Assignment2.jsx
@@ -8,9 +8,10 @@ export function Assignment2() {
     const [inputText, setInputText] = useState('');
 
     // Your code starts here
-    function showAlert() {
-
-    }
+  const showAlert=useCallback(function showAlert() {
+  
+    alert(inputText);
+   })  
     // Your code ends here
 
     return (
